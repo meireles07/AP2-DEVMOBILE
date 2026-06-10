7c20cf84b9d1430070f62015b93066f44ead3671
Bernardo Meireles <bmanjos07@gmail.com>
2026-06-10 12:54:29 -0300

    Criando o seufilme-api

---
+++ seufilme-api/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Banco de dados Sqlite
+SQLALCHEMY_DATABASE_URL = "sqlite:///./seufilme.db"
+
+# O engine é a "conexão" com o banco de dados
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}  # necessário para SQLite
+)
+
+# SessionLocal é o que usamos para abrir e fechar sessões com o banco
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base é a classe pai de todos os nossos modelos (tabelas)
+Base = declarative_base()
+
+# Função que abre uma sessão com o banco e fecha automaticamente ao terminar (Endpoint)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
